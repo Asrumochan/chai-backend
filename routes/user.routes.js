7e3f70568a77ff50c6798dd418948607906412c5
Asrumochan <asrumochanparida99@gmail.com>
2026-01-08 21:49:27 +0530

    Fix formatting and spacing in user routes for improved readability

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -38,19 +38,19 @@ router.route("/login").post(loginUser);
 // secured routes
 router.route("/logout").post(verifyJWT, logoutUser);
 router.route("/refresh-token").post(refreshAccessToken);
-router.route("/change-password").post(verifyJWT, changeCurrentPassword); 
+router.route("/change-password").post(verifyJWT, changeCurrentPassword);
 router.route("/current-user").get(verifyJWT, getCurrentUser);
 router.route("/update-account").patch(verifyJWT, updateAccountDetails);
 router
   .route("/update-avatar")
-  .post(verifyJWT, upload.single("avatar"), updateUserAvatar); 
+  .post(verifyJWT, upload.single("avatar"), updateUserAvatar);
 router.route("/remove-avatar").post(verifyJWT, removeUserAvatar);
 router
-.route("/update-cover-image")
-.patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage);
+  .route("/update-cover-image")
+  .patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage);
 router.route("/remove-cover-image").post(verifyJWT, removeUserCoverImage);
 
-router.route("/channel/:username").get(verifyJWT,getUserChannelProfile)
-router.route("/watch-history").get(verifyJWT,getWatchHistory)
+router.route("/channel/:username").get(verifyJWT, getUserChannelProfile);
+router.route("/watch-history").get(verifyJWT, getWatchHistory);
 
 export default router;
